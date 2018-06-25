a67d3a81396207df66cb4835ea406bc31fd5f8d6
garethosullivan <gareth.osullivan@icloud.com>
2018-06-25 08:16:23 +0100

    Initial commit

---
+++ Meme.V1/MemeCreatorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Meme.V1
+//
+//  Created by Gareth O'Sullivan on 25/06/2018.
+//  Copyright © 2018 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class MemeCreatorViewController: UIViewController {
+    
+    //MARK: - Application Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+    }
+
+}
+
